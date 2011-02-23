15b85f8e5d4d11f8a4302a12b993683f23132012
joaquin hernandez <joaquin.h.sandoval@gmail.com>
2011-02-23 13:43:01 -0600

    added prefix in phone_type_model

--- rolodex/app/models/phone_type_model.rb
+++ rolodex/app/models/phone_type_model.rb
@@ -1,4 +1,4 @@
 class PhoneTypeModel < ActiveRecord::Base
   has_many :phone_types
-  delegate :name, :to => :phone_number, :prefix => true
+  delegate :name, :to => :phone_number, :prefix => "phone_type"
 end
